e7e1d3ce6fca9748ccce391d484ac1ab3e5ac020
David <david.grech26@gmail.com>
2020-10-20 21:09:53 +0100

    Commit Augmented App

---
+++ AugmentedApp/AugmentedApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AugmentedApp.ViewModels;
+
+namespace AugmentedApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
